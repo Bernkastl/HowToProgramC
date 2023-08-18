92a1998bf6f4b50e79fe2bf81b5a4dd1cd585c0d
Bernkastel <valuableodai@gmail.com>
2023-08-19 00:56:48 +0300

    fixed formatting

--- Chapter 2/2_20.c
+++ Chapter 2/2_20.c
@@ -1,3 +1,12 @@
+/*
+* 2.20 (Circle Area, Diameter and Circumference) For a circle of radius 2, display the
+* diameter, circumference and area. Use the value 3.14159 for π. Use the following formulas
+* (r is the radius): diameter = 2r, circumference = 2πr and area = πr2. Perform each
+* of these calculations inside the printf statement(s) and use the conversion specification
+* %f. This chapter discussed only integer constants and variables. Chapter 3 will
+* discuss floating-point numbers—that is, values that can have decimal points.
+*/
+
 #include <stdio.h>
 
 int main(void)
