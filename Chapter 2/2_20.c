7b507cfc67df7ed3ff956e92d0daa8110a0463c3
Bernkastel <valuableodai@gmail.com>
2023-08-19 00:54:14 +0300

    updates

--- Chapter 2/2_20.c
+++ Chapter 2/2_20.c
@@ -2,4 +2,13 @@
 
 int main(void)
 {
+    int radius = 2;
+    float pi = 3.14159;
+
+    printf("A circle of radius %f\n", radius);
+    printf("Diameter is %f\n", 2.0 * radius);
+    printf("Circumference is %f\n", 2.0 * pi * radius);
+    printf("Area is %f\n", pi * radius * radius);
+
+    return 0;
 }
