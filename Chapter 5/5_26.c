f4dd68356a4e0053c4d26a7d829bba3e6067528b
Bernkastel <valuableodai@gmail.com>
2023-09-03 20:39:57 +0300

    Update 5_26.c
    code was optimized in expense of clarity, reverted

--- Chapter 5/5_26.c
+++ Chapter 5/5_26.c
@@ -28,15 +28,11 @@ bool isPerfect(int n)
 {
     int sum = 1;
 
-    for (int i = 2; i * i <= n; i++)
+    for (int i = 2; i < n; i++)
     {
         if (n % i == 0)
         {
             sum += i;
-            if (i != n / i)
-            {
-                sum += n / i;
-            }
         }
     }
 
