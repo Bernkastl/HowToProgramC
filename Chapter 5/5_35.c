64b23b5e5206336e4afbf39a1e7871f0fecb94cc
Bernkastel <valuableodai@gmail.com>
2023-09-04 17:04:16 +0300

    Create 5_35.c

---
+++ Chapter 5/5_35.c
@@ -0,0 +1,39 @@
+/*
+ * 5.35 (Fibonacci) The Fibonacci series
+ * 0, 1, 1, 2, 3, 5, 8, 13, 21, …
+ * begins with the terms 0 and 1 and has the property that each succeeding term is the
+ * sum of the two preceding terms. First, write a nonrecursive function fibonacci(n)
+ * that calculates the nth Fibonacci number. Use int for the function’s parameter and
+ * unsigned long long int for its return type. Then, determine the largest Fibonacci
+ * number that can be printed on your system.
+ */
+
+#include <stdio.h>
+
+unsigned long long fibonacci(int);
+
+int main(void)
+{
+    for (int i = 1; i <= 100000; i++)
+    {
+        printf("%llu\n", fibonacci(i));
+    }
+}
+
+unsigned long long fibonacci(int number)
+{
+    unsigned long long num1 = 0, num2 = 1, sum;
+
+    if (number == 1)
+        return 0;
+    else if (number == 2)
+        return 1;
+
+    for (int i = 3; i <= number; i++)
+    {
+        sum = num1 + num2;
+        num1 = num2;
+        num2 = sum;
+    }
+    return sum;
+}
